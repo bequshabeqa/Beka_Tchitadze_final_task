733a5a507a9078384df431a2f36b0e9f623e5117
bchitadze <beqachita@gmail.com>
2025-01-03 21:10:20 +0400

    davaredaqtire selenide-is davaleba

--- src/main/java/seleniumSteps/onlineShopSteps.java
+++
@@ -1,4 +0,0 @@
-package seleniumSteps;
-
-public class onlineShopSteps {
-}
--- src/test/java/SeleniumElements/onlineShopTest.java
+++ src/test/java/SeleniumElements/onlineShopTest.java
@@ -1,4 +1,42 @@
 package SeleniumElements;
 
-public class onlineShopTest {
+import Utils.SetUpClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import seleniumSteps.onlineShopRegSteps;
+
+public class onlineShopTest extends SetUpClass {
+
+    onlineShopRegSteps onlineShopRegSteps = new onlineShopRegSteps();
+
+    @BeforeSuite
+    public void suit() {
+        setUpGlobalWait();
+    }
+
+//    @Test
+//    public void RegNoSuccessTest() {
+//
+//        driver.get("https://practice.automationtesting.in/my-account/");
+//
+//        onlineShopRegSteps.emailInput("johndoe@example.com");
+//        onlineShopRegSteps.setPassword("Beqa123!@#");
+//        onlineShopRegSteps.clickLogInButton();
+//
+//        onlineShopRegSteps.checkFlashMessageErrorText(" An account is already registered with your email address. Please login.");
+//
+//    }
+
+    @Test
+    public void RegSuccessTest() {
+
+        driver.get("https://practice.automationtesting.in/my-account/");
+
+        onlineShopRegSteps.emailInput("bekatchitadze@gmail.com");
+        onlineShopRegSteps.setPassword("Beqa123!@#");
+        onlineShopRegSteps.clickLogInButton();
+
+//        onlineShopRegSteps.checkSuccessMessageText("My Account");
+
+    }
 }
